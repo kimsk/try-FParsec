e0da3d32e47a2797a74df97c8107c8d132768060
Karlkim Suwanmongkol <karlkim@gmail.com>
2022-12-28 16:29:07 -0600

    simplest lisp parser

---
+++ fslisp/Parser.fs
@@ -0,0 +1,59 @@
+module Parser
+
+open FParsec
+
+type Expr =
+    | Symbol of string
+    | Number of int
+    | String of string
+    | List of Expr list
+
+let validChar =
+    function
+    | c when c >= 'a' && c <= 'z' -> true
+    | c when c >= 'A' && c <= 'Z' -> true
+    | _ -> false
+
+let openParenthesis: Parser<_, unit> = skipChar '(' >>. spaces
+let closeParenthesis: Parser<_, unit> = spaces .>> skipChar ')'
+
+let symbol: Parser<Expr, unit> = many1Satisfy validChar |>> Symbol
+let number: Parser<Expr, unit> = pint32 |>> Number
+
+let str (s: string) = pstring s
+
+let stringLiteral: Parser<Expr, unit> =
+    let escape =
+        anyOf "\"\\/bfnrt"
+        |>> function
+            | 'b' -> "\b"
+            | 'f' -> "\u000C"
+            | 'n' -> "\n"
+            | 'r' -> "\r"
+            | 't' -> "\t"
+            | c -> string c // every other char is mapped to itself
+
+    let unicodeEscape =
+        /// converts a hex char ([0-9a-fA-F]) to its integer number (0-15)
+        let hex2int c = (int c &&& 15) + (int c >>> 6) * 9
+
+        str "u"
+        >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
+            (hex2int h3) * 4096 + (hex2int h2) * 256 + (hex2int h1) * 16 + hex2int h0
+            |> char
+            |> string)
+
+    let escapedCharSnippet = str "\\" >>. (escape <|> unicodeEscape)
+    let normalCharSnippet = manySatisfy (fun c -> c <> '"' && c <> '\\')
+
+    between (str "\"") (str "\"") (stringsSepBy normalCharSnippet escapedCharSnippet)
+    |>> String
+
+let expr, exprRef = createParserForwardedToRef<Expr, unit> ()
+
+
+let list = between openParenthesis closeParenthesis (sepBy expr spaces1 |>> List)
+
+do exprRef := choice [ symbol; number; stringLiteral; list ]
+
+let code = spaces >>. expr .>> spaces .>> eof
---
+++ fslisp/Program.fs
@@ -0,0 +1,20 @@
+﻿open FParsec
+
+open Parser
+
+let test parser input =
+    let result: ParserResult<_, unit> = run parser input
+    let padInput = input.PadRight 30
+
+    match result with
+    | Success(r, _, _) -> printfn "%s-> %O" padInput r
+    | Failure(e, _, _) -> printfn "%O" e
+
+// test nil "()"
+test symbol "abc"
+test number "123"
+test stringLiteral "\"TEST\""
+test list "(() a b 1 2 \"Hello\")"
+test list "((a b))"
+
+test code "((q 10) (list 1 2 3) a b 1 2 \"Hello\")"
